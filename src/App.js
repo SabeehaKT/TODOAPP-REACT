f73434bff1115aa11ba0eb882573c3ff50196077
SabeehaKT <kampalasabeeha@gmail.com>
2024-06-08 08:40:28 +0530

    TodoApp with React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddTodo from './Components/AddTodo';
+import SearchTodo from './Components/SearchTodo';
+import DeleteTodo from './Components/DeleteTodo';
+import ViewAll from './Components/ViewAll';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<AddTodo/>}/>
+      <Route path="/search" element={<SearchTodo/>}/>
+      <Route path="/delete" element={<DeleteTodo/>}/>
+      <Route path="/view" element={<ViewAll/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
